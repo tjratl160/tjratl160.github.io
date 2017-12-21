24714626d0cb7e6344f400f9fc7ac5906a6511e0
tjratl160 <ratliff.152@osu.edu>
2017-12-21 15:20:14 -0500

    Added image functionality and time stamp

--- fantasy/fantasy.js
+++ fantasy/fantasy.js
@@ -1,9 +1,43 @@
 window.onload=function() {
   // Month,Day,Year,Hour,Minute,Second
   upTime('sep,25,2017,23:00:00'); // ****** Change this line!
+  doPic();
+  doSucks();
 }
+
+function doPic(){
+  var picNum = Math.floor(Math.random() * 6)+ 1;
+  var picEl = document.getElementById('pic');
+  picEl.src = "images/"+picNum+".jpg"  
+}
+
+function doSucks(){
+  var pEl = document.getElementById("sucks");
+
+  var date = new Date()
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var am = hour/12 < 1;
+  
+  hour = hour % 12;
+  var minuteStr;
+  minuteStr = minute;
+  if(minute < 9){
+    minuteStr="0"+minute.toString();
+  }
+
+  var dateStr;
+  if(am){
+    dateStr = hour+":"+minuteStr+" AM";
+  }else{
+    dateStr = hour+":"+minuteStr+" PM";    
+  }
+  pEl.innerHTML = "It is "+dateStr+" and everyone still sucks at fantasy";
+}
+
 function upTime(countTo) {
   now = new Date();
+
   countTo = new Date(countTo);
   difference = (now-countTo);
 
